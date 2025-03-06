db8c596c37fb554261e2be79dd2e1a6b794d58c9
malay77patra <malay77patra@gmail.com>
2025-03-06 15:12:00 +0530

    testing

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const hookPath = path.join(__dirname, '../.git/hooks/pre-push');
+const hookPath = path.join(__dirname, '../.git/hooks/pre-commit');
 const hookContent = `#!/bin/sh
 node ./scripts/generate-example-env.js
 git add .env.example
@@ -14,12 +14,12 @@ function setupGitHook() {
     }
 
     if (fs.existsSync(hookPath)) {
-        console.log('pre-push hook already exists, make sure it contains the following lines:\n1. node ./scripts/generate-example-env.js\n2. git add .env.example');
+        console.log('pre-commit hook already exists, make sure it contains the following lines:\n1. node ./scripts/generate-example-env.js\n2. git add .env.example');
         return;
     }
 
     fs.writeFileSync(hookPath, hookContent, { mode: 0o755 });
-    console.log('pre-push hook installed successfully.');
+    console.log('pre-commit hook installed successfully.');
 }
 
 console.log('Setting up Git hook...');
